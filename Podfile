896861a9071edc031dd671d9b428eacdeb26b19a
Mateusz Łaś <mlasapk@gmail.com>
2021-12-17 00:14:47 +0100

    create

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '13.0'
+use_frameworks!
+
+target 'fbsdktest' do
+    pod 'FBSDKCoreKit'
+    pod 'FBSDKLoginKit'
+    pod 'FBSDKShareKit'
+end
---
+++ fbsdktest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fbsdktest
+//
+//  Created by Mateusz Łaś on 14/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
